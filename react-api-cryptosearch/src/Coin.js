e6368db5ebaf5f889b8dc5c392b752eacdefc327
dawsonhammdev <hammsdesigns@gmail.com>
2021-02-18 16:21:20 -0600

    updates.

--- react-api-cryptosearch/src/Coin.js
+++ react-api-cryptosearch/src/Coin.js
@@ -2,6 +2,7 @@ import React from 'react'
 import './Coin.css';
 
 const Coin = ({ image, name, symbol, price, volume, priceChange, marketcap}) => {
+    // hello
     return (
         <div className='coin-container'>
             <div className='coin-row'>
