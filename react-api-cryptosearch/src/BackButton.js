d2a96f3bfe8f96086f712b83f509718209dd162a
dawsonhammdev <hammsdesigns@gmail.com>
2021-06-21 13:42:52 -0500

    Removed dependency.

--- react-api-cryptosearch/src/BackButton.js
+++ react-api-cryptosearch/src/BackButton.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { useHistory } from "react-router-dom";
-import { BiArrowBack } from "react-icons/bi";
 
 export const Item = () => {
     let history = useHistory();
     return (
         <>
         <div className="back-btn-container">
-          <button onClick={() => history.goBack()}><BiArrowBack /></button>
+          <button onClick={() => history.goBack()}> Back </button>
         </div>
         </>
     );
